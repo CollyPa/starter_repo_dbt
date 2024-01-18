d84627e49fc5805eefc5084284d38071887f3b2e
CollyPa <colette.parak@gmail.com>
2024-01-18 13:12:56 +0100

    ctd

---
+++ models/mart/monthly_avg_temp.sql
@@ -0,0 +1,9 @@
+WITH total_month_avg as(
+    SELECT city, country, month, year, lat, lon
+    avg(avgtemp_c)as avg_temp_month, 
+    max(maxtemp_c)as max_temp_month, 
+    min(mintemp_c)as min_temp_month
+from {{ref("prep_temp")}}
+group by city, country, year, month, lat, lon
+)
+select * from total_month_avg
--- models/mart/weekly_avg_temp.sql
+++ models/mart/weekly_avg_temp.sql
@@ -1,6 +1,5 @@
 WITH total_avg as(
-    SELECT
-    city, country, year, lat, lon, 
+    SELECT city, country, week, year, lat, lon
     avg(avgtemp_c)as avg_temp_week, 
     max(maxtemp_c)as max_temp_week, 
     min(mintemp_c)as min_temp_week
